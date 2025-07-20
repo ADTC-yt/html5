12f11cbc073f32fb686881621c4cb275120bcf83
ADTC-yt <adtc.yt@outlook.com>
2025-07-20 16:59:15 +0530

    FrontPage completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.nav-links a');
+    button.addEventListener('hover', ()=>{
+       button.style.color ="brown"
+    })
+    button.addEventListener('mouseout',()=>{
+       button.style.color ="black"
+        
+    })
